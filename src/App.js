c776860b36125acf2325c14217ca0f4d19e76ebf
Abu thakir <abudx.at@gmail.com>
2022-08-23 10:30:51 +0530

    capstone project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Home from "./routes/home/home.component";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+const Shop = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Outlet></Outlet>
+      <h1>i am a shop page</h1>
+
     </div>
-  );
+
+  )
+}
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop></Shop>}></Route>
+      </Route>
+    </Routes>
+
+  )
 }
 
 export default App;
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import "./navigation.styles.scss";
+import { ReactComponent as CrownLogo } from "../../asserts/crown.svg";
+
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <span className="logo">
+                        <CrownLogo className="logo"></CrownLogo>
+                    </span>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="" to="/shop">
+                        shop
+                    </Link>
+                </div>
+            </div>
+            <Outlet></Outlet>
+        </Fragment>
+    )
+}
+
+
+export default Navigation;
